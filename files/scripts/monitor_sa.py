dc918b3426d4dcebd6473d6ad975f1f594c67302
Alex Batisse <alexandre.batisse@hey.com>
2025-11-13 19:59:16 +0100

    Update files/scripts/monitor_sa.py
    Co-authored-by: deusebio <enrico.deusebio@canonical.com>
    Signed-off-by: Alex Batisse <alexandre.batisse@hey.com>

--- files/scripts/monitor_sa.py
+++ files/scripts/monitor_sa.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-l",
         "--allowlist",
-        help="The service account allowlist path.",
+        help="The path of the file where the service account allowlist is specified.",
         type=str,
     )
     parser.add_argument(
