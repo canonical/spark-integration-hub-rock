e4ecb52bfae9bbddcceeb34c36fb8c553ac3227e
Paolo Sottovia <paolo.sottovia@canonical.com>
2026-02-25 17:59:44 +0000

    wip: add truststore support and tmp release

--- files/scripts/monitor_sa.py
+++ files/scripts/monitor_sa.py
@@ -5,6 +5,7 @@
 """Routine that updates secrets for Spark service accounts."""
 
 import argparse
+import base64
 import fnmatch
 import logging
 import os
@@ -15,6 +16,7 @@ from typing import NamedTuple, cast
 
 from lightkube.core.client import Client, LabelValue
 from lightkube.core.exceptions import ApiError
+from lightkube.models.meta_v1 import ObjectMeta
 from lightkube.resources.core_v1 import Secret, ServiceAccount
 from spark8t.domain import PropertyFile
 from spark8t.literals import HUB_LABEL
@@ -72,6 +74,28 @@ def is_allowed(
     )
 
 
+def create_secret_from_file(secret_name: str, file_path: Path, namespace: str) -> Secret:
+    """Create a Kubernetes Secret object from a file."""
+    # Read the file content
+    with file_path.open("rb") as f:
+        file_content = f.read()
+
+    # The output needs to be a decoded utf-8 string for the JSON serialization
+    encoded_content = base64.b64encode(file_content).decode("utf-8")
+
+    # Extract the filename to use as the key (default kubectl behavior)
+    file_key = os.path.basename(file_path)
+
+    # Construct the Secret object
+    secret = Secret(
+        metadata=ObjectMeta(name=secret_name, namespace=namespace),
+        type="Opaque",
+        data={file_key: encoded_content},
+    )
+
+    return secret
+
+
 if __name__ == "__main__":
     logger.info("Start process")
     parser = argparse.ArgumentParser(
@@ -104,11 +128,18 @@ if __name__ == "__main__":
         default=TIMEOUT_DEFAULT_SECONDS,
         type=int,
     )
+    parser.add_argument(
+        "-s",
+        "--truststore",
+        help="The path of the truststore file.",
+        type=str,
+    )
     args = parser.parse_args()
     logger.info("Start process that update service account secrets.")
     client = Client(field_manager=args.app_name)  # type: ignore
     label_selector: dict[str, LabelValue] = {"app.kubernetes.io/managed-by": "spark8t"}
     allowlist_path = Path(args.allowlist)
+    truststore_path = Path(args.truststore) if args.truststore else None
     try:
         with allowlist_path.open("r") as f:
             allowlist = [entry.strip() for entry in f.read().splitlines()]
@@ -150,11 +181,17 @@ if __name__ == "__main__":
             logger.info("Empty configuration. No secret to update.")
 
         secret_name = f"{HUB_LABEL}-{sa_name}"
+        secret_name_truststore = f"{HUB_LABEL}-{sa_name}-truststore"
         # if secret is already there, delete it.
         try:
             s = client.get(Secret, name=secret_name, namespace=namespace)
             print(f"retrieved secrets: {s}")
             client.delete(Secret, name=secret_name, namespace=namespace)
+            # update trustore secret if the file path is provided in the configuration.
+            if truststore_path:
+                s_truststore = client.get(Secret, name=secret_name_truststore, namespace=namespace)
+                print(f"retrieved truststore secrets: {s_truststore}")
+                client.delete(Secret, name=secret_name_truststore, namespace=namespace)
         except ApiError as e:
             logger.info(f"Api error: {e}")
 
@@ -177,4 +214,13 @@ if __name__ == "__main__":
         )
         # Create secret
         client.create(s)
+
+        # Create secret for truststore if the file path is provided in the configuration.
+        if truststore_path:
+            logger.info(f"Updating secret for truststore: {secret_name_truststore}")
+            truststore_secret = create_secret_from_file(
+                secret_name_truststore, truststore_path, namespace
+            )
+            client.create(truststore_secret)
+
         logger.info("--------------------------------------------------------")
